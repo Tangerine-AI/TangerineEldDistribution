da58c011871ec6e9f376034dc15875a08adf9b01
reenu-tangerine <reenu.deswal@tangerine.ai>
2020-10-15 13:33:03 +0530

    initial commit for .framework

---
+++ TangerineEld.framework/Headers/TangerineEld.h
@@ -0,0 +1,19 @@
+//
+//  TangerineEld.h
+//  TangerineEld
+//
+//  Created by MAC PRO on 17/09/20.
+//  Copyright © 2020 MAC PRO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TangerineEld.
+FOUNDATION_EXPORT double TangerineEldVersionNumber;
+
+//! Project version string for TangerineEld.
+FOUNDATION_EXPORT const unsigned char TangerineEldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TangerineEld/PublicHeader.h>
+
+
